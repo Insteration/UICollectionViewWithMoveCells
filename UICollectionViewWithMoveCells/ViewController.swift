7ebc8a12ef59f30cb09f216e490fbd38ce58f8cb
Artem <inste@icloud.com>
2019-06-12 19:56:54 +0300

    Initial Commit

---
+++ UICollectionViewWithMoveCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewWithMoveCells
+//
+//  Created by Artem Karmaz on 12.06.2019.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
